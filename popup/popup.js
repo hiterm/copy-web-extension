9a2078a02628fc9c48eed7bd636801c0c9d18795
htlsne <htlsne23@gmail.com>
2021-12-22 00:55:42 +0900

    Rewrite a copy button with react

--- popup/popup.js
+++
@@ -1,8 +0,0 @@
-const copyMarkdownLink = async () => {
-  const tabs = await browser.tabs.query({currentWindow: true, active: true});
-  const tab = tabs[0];
-  const markdownLink = `[${tab.title}](${tab.url})`;
-  navigator.clipboard.writeText(markdownLink);
-};
-const copyMarkdownButton = document.querySelector('#copy-markdown-button');
-copyMarkdownButton.addEventListener('click', copyMarkdownLink);
--- popup/popup.tsx
+++ popup/popup.tsx
@@ -1,5 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import browser from 'webextension-polyfill';
 
-const App: React.VFC = () => (<div>Hello</div>);
+const copyMarkdownLink = async () => {
+  const tabs = await browser.tabs.query({ currentWindow: true, active: true });
+  const tab = tabs[0];
+  const markdownLink = `[${tab.title}](${tab.url})`;
+  navigator.clipboard.writeText(markdownLink);
+};
+
+const App: React.VFC = () => (<button onClick={copyMarkdownLink}>Copy markdown link</button>);
+
 ReactDOM.render(<App />, document.getElementById('root'));
