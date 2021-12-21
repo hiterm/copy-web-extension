5318c0ae7ba0e5faf7bcc8ebf269689ccf3c5bbc
htlsne <htlsne23@gmail.com>
2021-12-22 00:42:26 +0900

    Copy manifest.json, etc.

--- build.ts
+++ build.ts
@@ -1,7 +1,12 @@
 import { build } from 'esbuild';
+import { promises as fs } from 'fs';
 
 build({
   entryPoints: ['popup/popup.tsx'],
   bundle: true,
   outdir: 'dist/popup',
 });
+
+fs.copyFile('popup/popup.html', 'dist/popup/popup.html');
+fs.copyFile('manifest.json', 'dist/manifest.json');
+fs.cp('icons', 'dist/icons', { recursive: true });
