f1fc39e20f437b4f70e290b3045c40384bad105a
htlsne <htlsne23@gmail.com>
2021-12-23 21:57:52 +0900

    Add eslint plugins

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,14 @@ module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier',
+  ],
   env: {
     node: true,
   },
