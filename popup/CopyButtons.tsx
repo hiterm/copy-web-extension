390617aec59e153dbb0b9d752d347a292cb667f8
htlsne <htlsne23@gmail.com>
2021-12-23 22:15:00 +0900

    Add a Jira link button

--- popup/CopyButtons.tsx
+++ popup/CopyButtons.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import browser from 'webextension-polyfill';
-import { Box, Button } from '@chakra-ui/react';
+import { Box, Button, Stack } from '@chakra-ui/react';
 
 const copyMarkdownLink = async () => {
   const tabs = await browser.tabs.query({ currentWindow: true, active: true });
@@ -9,9 +9,22 @@ const copyMarkdownLink = async () => {
   navigator.clipboard.writeText(markdownLink);
 };
 
+const copyJiraLink = async () => {
+  const tabs = await browser.tabs.query({ currentWindow: true, active: true });
+  const tab = tabs[0];
+  const jiraLink = `[${tab.title}|${tab.url}]`;
+  navigator.clipboard.writeText(jiraLink);
+};
+
 export const CopyButtons: React.VFC = () => (
   <Box sx={{ margin: 1 }}>
-    <Button colorScheme="teal" onClick={copyMarkdownLink}>Copy markdown link</Button>
+    <Stack direction="column">
+      <Button colorScheme="teal" onClick={copyMarkdownLink}>
+        Copy markdown link
+      </Button>
+      <Button colorScheme="teal" onClick={copyJiraLink}>
+        Copy Jira link
+      </Button>
+    </Stack>
   </Box>
 );
-
