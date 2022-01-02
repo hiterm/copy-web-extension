5f9ce9d87d5e3144161f524e65d6d294285a51cd
htlsne <htlsne23@gmail.com>
2022-01-02 12:59:48 +0900

    Use current title and url

--- popup/CopyHtmlLinkButton.tsx
+++ popup/CopyHtmlLinkButton.tsx
@@ -1,10 +1,22 @@
 import { Button } from '@chakra-ui/react';
 import React, { useRef } from 'react';
+import browser from 'webextension-polyfill';
 
 export const CopyHtmlLinkButton: React.VFC = () => {
   const aRef = useRef<HTMLAnchorElement>(null!);
 
-  const handleClick = () => {
+  const handleClick = async () => {
+    const tabs = await browser.tabs.query({
+      currentWindow: true,
+      active: true,
+    });
+    const tab = tabs[0];
+    const title = tab.title;
+    const url = tab.url;
+
+    aRef.current.textContent = title!;
+    aRef.current.href = url!;
+
     const range = document.createRange();
     range.selectNode(aRef.current);
     const selection = window.getSelection()!;
