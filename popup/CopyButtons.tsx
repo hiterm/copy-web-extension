1b7c7bfb8d4c4b4f3ed6ead8b899ec8e812f62fd
htlsne <htlsne23@gmail.com>
2022-01-22 19:36:00 +0900

    Show popover on copied

--- popup/CopyButtons.tsx
+++ popup/CopyButtons.tsx
@@ -1,6 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import browser from 'webextension-polyfill';
-import { Box, Button, Stack } from '@chakra-ui/react';
+import {
+  Box,
+  Button,
+  Popover,
+  PopoverArrow,
+  PopoverCloseButton,
+  PopoverContent,
+  PopoverHeader,
+  PopoverTrigger,
+  Stack,
+} from '@chakra-ui/react';
 import { CopyHtmlLinkButton } from './CopyHtmlLinkButton';
 
 const copyMarkdownUrl = async () => {
@@ -17,12 +27,37 @@ const copyJiraUrl = async () => {
   navigator.clipboard.writeText(jiraLink);
 };
 
+const CopyMarkdownLinkButton: React.VFC = () => {
+  const [open, setOpen] = useState(false);
+
+  const handleClick = async () => {
+    await copyMarkdownUrl();
+    setOpen(true);
+    setTimeout(() => {
+      setOpen(false);
+    }, 2000);
+  };
+
+  return (
+    <Popover isOpen={open}>
+      <PopoverTrigger>
+        <Button colorScheme="teal" onClick={handleClick}>
+          Copy markdown link
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent sx={{ width: 100 }}>
+        <PopoverArrow />
+        <PopoverCloseButton />
+        <PopoverHeader>Copied!</PopoverHeader>
+      </PopoverContent>
+    </Popover>
+  );
+};
+
 export const CopyButtons: React.VFC = () => (
   <Box sx={{ margin: 1 }}>
     <Stack direction="column">
-      <Button colorScheme="teal" onClick={copyMarkdownUrl}>
-        Copy markdown link
-      </Button>
+      <CopyMarkdownLinkButton />
       <Button colorScheme="teal" onClick={copyJiraUrl}>
         Copy Jira link
       </Button>
