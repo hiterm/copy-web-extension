8e7a7eb8048a7b25a4f53a1a2616780594460f10
hiterm <hiterm23@gmail.com>
2022-10-14 00:58:36 +0900

    Rewrite markdown button with Mantine

--- popup/CopyButtons.tsx
+++ popup/CopyButtons.tsx
@@ -3,12 +3,14 @@ import browser from 'webextension-polyfill';
 import { Box, Stack } from '@chakra-ui/react';
 import { CopyHtmlLinkButton } from './CopyHtmlLinkButton';
 import { ButtonWithPopover } from './ButtonWithPopover';
+import { Button } from '@mantine/core';
+import { useClipboard } from '@mantine/hooks';
 
-const copyMarkdownUrl = async () => {
+const getMarkdownUrl = async (): Promise<string> => {
   const tabs = await browser.tabs.query({ currentWindow: true, active: true });
   const tab = tabs[0];
   const markdownLink = `[${tab.title}](${tab.url})`;
-  navigator.clipboard.writeText(markdownLink);
+  return markdownLink;
 };
 
 const copyJiraUrl = async () => {
@@ -18,16 +20,23 @@ const copyJiraUrl = async () => {
   navigator.clipboard.writeText(jiraLink);
 };
 
-export const CopyButtons: React.VFC = () => (
-  <Box sx={{ margin: 1 }}>
-    <Stack direction="column">
-      <ButtonWithPopover onClick={copyMarkdownUrl}>
-        Copy Markdown link
-      </ButtonWithPopover>
-      <ButtonWithPopover onClick={copyJiraUrl}>
-        Copy Jira link
-      </ButtonWithPopover>
-      <CopyHtmlLinkButton />
-    </Stack>
-  </Box>
-);
+export const CopyButtons: React.VFC = () => {
+  const clipboard = useClipboard({ timeout: 1000 });
+
+  return (
+    <Box sx={{ margin: 1 }}>
+      <Stack direction="column">
+        <Button
+          color={clipboard.copied ? 'teal' : 'blue'}
+          onClick={async () => clipboard.copy(await getMarkdownUrl())}
+        >
+          {clipboard.copied ? 'Copied' : 'Copy Markdown link'}
+        </Button>
+        <ButtonWithPopover onClick={copyJiraUrl}>
+          Copy Jira link
+        </ButtonWithPopover>
+        <CopyHtmlLinkButton />
+      </Stack>
+    </Box>
+  );
+};
--- popup/Popup.tsx
+++ popup/Popup.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { ChakraProvider } from '@chakra-ui/react';
 import { CopyButtons } from './CopyButtons';
+import { MantineProvider } from '@mantine/core';
 
 export const Popup: React.VFC = () => (
-  <ChakraProvider>
-    <CopyButtons />
-  </ChakraProvider>
+  <MantineProvider withGlobalStyles withNormalizeCSS>
+    <ChakraProvider>
+      <CopyButtons />
+    </ChakraProvider>
+  </MantineProvider>
 );
