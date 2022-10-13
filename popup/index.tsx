01a7c4b9875c69d62640e9af70151168ecbc88ee
hiterm <hiterm23@gmail.com>
2022-10-14 00:36:50 +0900

    Update render method

--- popup/index.tsx
+++ popup/index.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { Popup } from './Popup';
 
-ReactDOM.render(<Popup />, document.getElementById('root'));
+const container = document.getElementById('root');
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+const root = createRoot(container!);
+root.render(<Popup />);
